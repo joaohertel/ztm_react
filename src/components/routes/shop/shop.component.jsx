a711d9f32589a4c32b67eaa151961e7e338011b6
unknown <joaohertels@gmail.com>
2023-11-03 21:16:26 -0300

    feat: Add Product Card component and styles to self and shop.component

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,16 +1,16 @@
 import { useContext } from "react";
 import { ProductContext } from "../../contexts/product.context";
+import { ProductCard } from "../../product-card/product-card.component";
+import './shop.styles.scss';
 
 export const Shop = () => {
   const productsState = useContext(ProductContext);
   const { products } = productsState;
 
   return (
-    <div>
-      {products.map((elem) => (
-        <div key={elem.id}>
-          <p>{elem.name}</p>
-        </div>
+    <div className='products-container'>
+      {products.map((product) => (
+          <ProductCard key={product.id} product={product}></ProductCard>
       ))}
     </div>
   );
